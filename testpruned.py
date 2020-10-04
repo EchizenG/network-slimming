23635659f7315b0c8b7d269f268270079309fe39
group5 <gaoyang9180@gmail.com>
2020-10-04 12:10:26 -0400

    delete unnecessary parameter

--- testpruned.py
+++ testpruned.py
@@ -10,8 +10,6 @@ from models import *
 
 # Prune settings
 parser = argparse.ArgumentParser(description='PyTorch Slimming CIFAR prune')
-parser.add_argument('--dataset', type=str, default='cifar100',
-                    help='training dataset (default: cifar10)')
 parser.add_argument('--test-batch-size', type=int, default=256, metavar='N',
                     help='input batch size for testing (default: 256)')
 parser.add_argument('--no-cuda', action='store_true', default=False,
@@ -22,23 +20,14 @@ parser.add_argument('--percent', type=float, default=0.5,
                     help='scale sparse rate (default: 0.5)')
 parser.add_argument('--model', default='', type=str, metavar='PATH',
                     help='path to the model (default: none)')
-parser.add_argument('--save', default='', type=str, metavar='PATH',
-                    help='path to save pruned model (default: none)')
 args = parser.parse_args()
 args.cuda = not args.no_cuda and torch.cuda.is_available()
 
 def test(model):
     time_start = time.time()
     kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
-    if args.dataset == 'cifar10':
         test_loader = torch.utils.data.DataLoader(
-            datasets.CIFAR10('./data.cifar10', train=False, transform=transforms.Compose([
-                transforms.ToTensor(),
-                transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])),
-            batch_size=args.test_batch_size, shuffle=True, **kwargs)
-    elif args.dataset == 'cifar100':
-        test_loader = torch.utils.data.DataLoader(
-            datasets.CIFAR100('./data.cifar100', train=False, transform=transforms.Compose([
+            datasets.CIFAR100('./data.cifar100',download=True, train=False, transform=transforms.Compose([
                 transforms.ToTensor(),
                 transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])),
             batch_size=args.test_batch_size, shuffle=True, **kwargs)
@@ -58,9 +47,6 @@ def test(model):
     print('\nTest set: Accuracy: {}/{} ({:.1f}%) Consuming Time: {}\n'.format(
         correct, len(test_loader.dataset), 100. * correct / len(test_loader.dataset), time_end-time_start))
     return correct / float(len(test_loader.dataset))
-
-if not os.path.exists(args.save):
-    os.makedirs(args.save)
 
 if args.model:
     if os.path.isfile(args.model):
